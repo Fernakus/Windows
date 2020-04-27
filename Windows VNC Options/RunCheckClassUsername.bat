66087a493ffdf1ecb9259933a4ef3972f1b79b1c
Matt Ferlaino <35575372+Fernakus@users.noreply.github.com>
2020-04-27 12:49:39 -0400

    Delete RunCheckClassUsername.bat

--- Windows VNC Options/RunCheckClassUsername.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f "tokens=4,5 delims=^=." %%A IN ('find "host=" %*') do set c=%%A.%%B
-echo.
-echo ip address for this machine is 192.168.%c%
-
-\\citydata\public\general\it\elevate C:\VNCRightClickOptions\CheckClassUsernames.bat 192.168.%c%
